a0b7c4c67d5527aba554d2199a89f2a759826016
Zeinab Hassan <zeinab.hassan101@gmail.com>
2017-05-05 13:54:42 -0500

    Base Mode Commit

--- public/views/client.js
+++ public/views/client.js
@@ -1,11 +1,8 @@
 $(document).ready( function (){
   console.log('SOURCE JQ');
+  getAlbums();
   $('#addAlbum').on('click', sendAlbum);
 });
-
-
-
-
 
 var sendAlbum = function(){
   var albumToSend = {
@@ -16,12 +13,35 @@ var sendAlbum = function(){
   }; //end of var albumToSend
   console.log('sending: ', albumToSend);
 
-$.ajax({
-  type: 'POST',
-  url: '/album',
-  data: albumToSend,
-  success: function(response) {
-    console.log('back with: ', response);
-  }//end success
-});//end ajax
+  $.ajax({
+    type: 'POST',
+    url: '/album',
+    data: albumToSend,
+    success: function(response) {
+      console.log('back with: ', response);
+    }//end success
+  });//end ajax
+  getAlbums();
 };//end of sendAlbum
+
+function getAlbums(){
+  console.log('in get albums');
+  $.ajax({
+    type: 'GET',
+    url: '/album',
+    success: function(response) {
+      console.log('back from get albums with: ', response);
+      var outputDiv = $('#outputDiv');
+      outputDiv.empty();
+
+      for (var i = 0; i < response.length; i++) {
+        var cellText = '<div class="col-sm-3 music"><img src=" '+ response[i].albumImg +' " style="width:100%"/>';
+        cellText+= "<p>" + response[i].albumName +"</p>";
+        cellText+= "<p>" + response[i].albumArtist +"</p>";
+        cellText+= "<p>" + response[i].albumYear +"</p>";
+
+        outputDiv.append(cellText);
+      }//end for var
+    }//end success
+  });//end of ajax
+}//end getAlbums function
